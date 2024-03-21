50a3fdf626bfdee5166c5b99b06599fafb16cfdd
Pavel Perikov <perikov@gmail.com>
2024-03-21 18:05:03 +0400

    method generation

--- src/main/scala/com/perikov/cassandra/macros/PrinterDeriving.scala
+++ src/main/scala/com/perikov/cassandra/macros/PrinterDeriving.scala
@@ -6,25 +6,28 @@ import scala.annotation.experimental
 @experimental
 transparent inline def derivePrinting[T]: T = ${ derivePrintingImpl[T] }
 
-/** @see
-  *   https://stackoverflow.com/questions/68550985/method-override-with-scala-3-macros
-  */
 @experimental
 def derivePrintingImpl[T: Type](using Quotes) =
   import quotes.reflect.*
 
-  val parents = List(TypeTree.of[Object], TypeTree.of[T])
+  val traitTypeTree = TypeTree.of[T]
+  val traitMethods  = traitTypeTree.symbol.declaredMethods
+  val parents       = List(TypeTree.of[Object], traitTypeTree)
 
-  def decls(cls: Symbol) = List(
-    Symbol.newMethod(
-      cls,
-      "f1",
-      MethodType(List("g", "a"))(
-        _ => List(TypeRepr.of[Int], TypeRepr.of[String]),
-        _ => TypeRepr.of[String]
-      )
-    )
-  )
+  def decls(cls: Symbol) =
+    traitMethods.map(_.tree).collect {
+      case d @ DefDef(name, paramClauses, resType, None) =>
+        if paramClauses.size > 1 then
+          report.errorAndAbort(
+            s"Only one parameter list is supported for method $name"
+          )
+        val params = paramClauses.head.params
+        val mtype  = MethodType(params.map(_.name))(
+          _ => params.collect { case ValDef(name, tpt, _) => tpt.tpe },
+          _ => resType.tpe
+        )
+        Symbol.newMethod(cls, name, mtype)
+    }
 
   val classSymbol = Symbol.newClass(
     Symbol.spliceOwner,
@@ -33,17 +36,22 @@ def derivePrintingImpl[T: Type](using Quotes) =
     decls,
     None
   )
-  def f1Sym = classSymbol.declaredMethod("f1").head
-  def f1Def = DefDef(f1Sym,  _ => Some('{"Hello"}.asTerm))
+  def definitions = classSymbol.declaredMethods.map(sym =>
+    DefDef(sym, args => {
+      val methName = Expr(sym.name)
+      val t = Expr.ofList( args.head.map(_.asExpr))
+      Some('{ s"${$methName}(${$t.mkString(", ")})" }.asTerm)
+    })
+  )
 
-  val classDef = ClassDef(classSymbol, parents, List(f1Def))
+  val classDef = ClassDef(classSymbol, parents, definitions)
 
-  val newCls   = Typed(
+  val newCls = Typed(
     Apply(
       Select(New(TypeIdent(classSymbol)), classSymbol.primaryConstructor),
       Nil
     ),
-    TypeTree.of[T]
+    traitTypeTree
   )
 
   val block = Block(List(classDef), newCls)
--- src/test/scala/com/perikov/cassandra/macros/PrinterDeriving.scala
+++ src/test/scala/com/perikov/cassandra/macros/PrinterDeriving.scala
@@ -8,13 +8,11 @@ class PrinterDeriving extends munit.FunSuite:
   test("deriving printing") {
     trait Abc:
       def f1(g: Int, a: String): String
-      // def f2(t: Float, a: String): String
+      def f2(t: Float, a: String): String
     end Abc
 
     val a: Abc= derivePrinting[Abc]
-    assertEquals(a.f1(10,"s"),"Hello")
-
-    // assertEquals(a.f1(10, "SDF"), "f1(10, \"SDF\")")
-    // assertEquals(a.f2(0f, "SDF"), "f2(0.0, \"SDF\")")
+    assertEquals(a.f1(10,"s"),"f1(10, s)")
+    assertEquals(a.f2(0f,"s"),"f2(0.0, s)")
 
   }
