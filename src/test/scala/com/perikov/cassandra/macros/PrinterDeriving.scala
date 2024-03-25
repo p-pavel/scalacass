d392c53af1af1ef95bd1f3c83ba2ba6fbfd04692
Pavel Perikov <perikov@gmail.com>
2024-03-25 19:32:13 +0400

    formatting

--- src/test/scala/com/perikov/cassandra/macros/PrinterDeriving.scala
+++ src/test/scala/com/perikov/cassandra/macros/PrinterDeriving.scala
@@ -2,7 +2,6 @@ package com.perikov.cassandra.macros
 
 import scala.annotation.experimental
 
-
 @experimental
 class PrinterDeriving extends munit.FunSuite:
   trait Abc:
@@ -11,23 +10,22 @@ class PrinterDeriving extends munit.FunSuite:
   end Abc
 
   test("deriving printing") {
-    val a: Abc= derivePrinting[Abc]
-    assertEquals(a.f1(10,"s"),"f1(10, s)")
-    assertEquals(a.f2(0f,"s"),"f2(0.0, s)")
+    val a: Abc = derivePrinting[Abc]
+    assertEquals(a.f1(10, "s"), "f1(10, s)")
+    assertEquals(a.f2(0f, "s"), "f2(0.0, s)")
   }
 
   test("type aliasing") {
     type T = Abc
     val a: T = derivePrinting[T]
-    assertEquals(a.f1(10,"s"),"f1(10, s)")
+    assertEquals(a.f1(10, "s"), "f1(10, s)")
   }
 
   test("tagless final") {
     trait Q[T]:
       def f1(g: Int, a: String): T
-    
+
     val a: Q[String] = derivePrinting[Q[String]]
   }
 
 end PrinterDeriving
-
