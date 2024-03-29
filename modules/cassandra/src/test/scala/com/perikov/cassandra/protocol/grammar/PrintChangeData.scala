1c518158735f2641d8a08d406293e9a5325ed5d5
Pavel Perikov <perikov@gmail.com>
2024-03-30 02:12:54 +0400

    tests polishing

--- modules/cassandra/src/test/scala/com/perikov/cassandra/protocol/grammar/PrintChangeData.scala
+++ modules/cassandra/src/test/scala/com/perikov/cassandra/protocol/grammar/PrintChangeData.scala
@@ -13,11 +13,11 @@ class PrintChangeData extends munit.FunSuite:
   test("print change data") {
     val changeDataPrinter = derivePrinting[Prt]
     val args              = Seq("ks", "obj", "types")
-    val it                = args.iterator;
+    val it                = args.iterator
 
     inline given changeDataPrinter.string = it.next()
 
     val res = "AGGREGATE".dispatcherByMethodName(changeDataPrinter)
-    assertEquals(res, "AGGREGATE" ++ args.mkString("(", ", ", ")"))
+    assertEquals(res, args.mkString("AGGREGATE(", ", ", ")"))
   }
 end PrintChangeData
