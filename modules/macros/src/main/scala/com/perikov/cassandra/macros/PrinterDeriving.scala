d49df304a1a11835a097cdc2f7a5fe5f5083cd1e
Pavel Perikov <perikov@gmail.com>
2024-03-29 06:33:12 +0400

    scaladoc

--- modules/macros/src/main/scala/com/perikov/cassandra/macros/PrinterDeriving.scala
+++ modules/macros/src/main/scala/com/perikov/cassandra/macros/PrinterDeriving.scala
@@ -3,6 +3,32 @@ package com.perikov.cassandra.macros
 import scala.quoted.*
 import scala.annotation.experimental
 
+/** Tries to derive a printer implementation for trait T Can be used for
+  * tagless-final style traits.
+  * {{{
+  * trait Abc[T]:
+  *   def f1(g: Int, a: String): T
+  *
+  * derivePrinting[Abc[String]]
+  * }}}
+  * Will produce an instance of class inherited from `Abc[String]` with `f1`
+  * implemented as
+  * {{{
+  * class Printer extends Abc[String]:
+  *   override def f1(g: Int, a: String): String = s"f1($g, $a)"
+  * }}}
+  *
+  * abstract type members are also supported
+  *
+  * @note
+  *   the type parameter `T` should be a class type and not a refined type. Type
+  *   aliases are followed though
+  *
+  * @tparam T
+  *   the trait to derive a printer for
+  * @return
+  *   The implementation of `T`
+  */
 @experimental
 transparent inline def derivePrinting[T]: T = ${ derivePrintingImpl[T] }
 
