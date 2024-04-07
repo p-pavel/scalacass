cb30749ae90be57314503ab567569cc5cc520170
Pavel Perikov <perikov@gmail.com>
2024-04-07 13:30:57 +0400

    scala version bump

--- project/Plugin.scala
+++ project/Plugin.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object MyPlugin extends AutoPlugin {
   override def trigger: PluginTrigger = allRequirements
   override lazy val projectSettings = Seq(
-    scalaVersion := "3.4.0",
+    scalaVersion := "3.4.1",
     scalacOptions ++= Seq("-explain", "-deprecation",  "-source:future", "-rewrite" , "-Ykind-projector"),
   )
 }
